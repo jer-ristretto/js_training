81bc08f3f182818374498e675942ad3f71f5cb07
Jeremy Xiao <s3829221@student.rmit.edu.au>
2022-01-16 17:13:23 +1100

    Adjust indentation

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let count = 0;
 
 function increment() {
-    count += 1;
-    document.getElementById("count-el").innerText = count;
+  count += 1;
+  document.getElementById("count-el").innerText = count;
 }
 
 let messageToUserEl = document.getElementById("messageToUser");
 messageToUserEl.innerText = "Previous entries:";
 
 function save() {
-    messageToUserEl.textContent += count + " - ";
+  messageToUserEl.textContent += count + " - ";
 }
